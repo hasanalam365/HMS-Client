59894b35adb53283cb549e866605c80696a64279
hasanalam24 <hasanalam365@gmail.com>
2024-07-08 07:18:34 +0600

    useProducts data fetch and show new arrival

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,19 @@ import router from './Routes/Routes.jsx';
 import AuthProvider from './firebase/AuthProvider.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
-      <ToastContainer></ToastContainer>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer></ToastContainer>
+      </QueryClientProvider>
+
     </AuthProvider>
   </React.StrictMode>,
 )
