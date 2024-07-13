5a2b6cf12d7a18b042a25d94517bebe34a31417b
hasanalam24 <hasanalam365@gmail.com>
2024-07-13 13:10:43 +0600

    all users page  user change role and delete user done

--- src/Dashboard/Dashboard/AllUsers.jsx
+++ src/Dashboard/Dashboard/AllUsers.jsx
@@ -1,17 +1,77 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+import { FaUsers } from "react-icons/fa";
 
 const AllUsers = () => {
 
     const axiosSecure = useAxiosSecure()
 
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users')
             return res.data
         }
     })
+
+
+    const handleChangeRole = (user) => {
+        Swal.fire({
+            title: "Do you want to change user role?",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: `Make Admin`,
+            denyButtonText: `Make User`
+        }).then(async (result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                const role = 'admin'
+                const res = await axiosSecure.patch(`/users/admin/${user.email}`, { role })
+
+                if (res.data.modifiedCount === 1) {
+                    toast('Make Admin successfully')
+                    refetch()
+                }
+            } else if (result.isDenied) {
+                const role = 'user'
+                const res = await axiosSecure.patch(`/users/admin/${user.email}`, { role })
+
+                if (res.data.modifiedCount === 1) {
+                    toast('Make user successfully')
+                    refetch()
+                }
+            }
+        });
+    }
+
+
+    const handleDelete = (email) => {
+
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to delete this user!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/users/${email}`)
+
+                if (res.data.deletedCount === 1) {
+                    toast('User has been deleted')
+                    refetch()
+                }
+            }
+        });
+
+
+    }
+
 
     return (
         <div className="flex flex-col mt-4 px-4 md:p-8">
@@ -25,7 +85,7 @@ const AllUsers = () => {
                     <thead>
                         <tr>
 
-                            <th>No</th>
+                            <th>Photo</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Role</th>
@@ -41,9 +101,7 @@ const AllUsers = () => {
                                         <div className="flex items-center gap-3">
                                             <div className="avatar">
                                                 <div className="mask mask-squircle h-12 w-12">
-                                                    <img
-                                                        src={user.photoURL}
-                                                        alt='user photo' />
+                                                    <img src={user.photoURL} alt='user photo' />
                                                 </div>
                                             </div>
 
@@ -52,9 +110,15 @@ const AllUsers = () => {
 
                                     <td>{user.displayName}</td>
                                     <td>{user.email}</td>
-                                    <td>{user.role}</td>
+                                    <td>
+                                        {user.role === 'admin' ? <button onClick={() => handleChangeRole(user)}>
+                                            Admin
+                                        </button> : <button onClick={() => handleChangeRole(user)} className="btn btn-ghost btn-xs text-white bg-green-600 ">
+                                            <FaUsers className="text-lg" />
+                                        </button>}
+                                    </td>
                                     <th>
-                                        <button className="btn btn-ghost btn-xs">details</button>
+                                        <button onClick={() => handleDelete(user.email)} className="btn btn-ghost btn-xs text-white bg-red-600 ">Delete</button>
                                     </th>
                                 </tr>)
                         }
