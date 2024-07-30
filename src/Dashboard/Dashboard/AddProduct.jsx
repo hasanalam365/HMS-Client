d5c07fe8d7ae2405871cb47713b17f83c73164a9
hasanalam24 <hasanalam365@gmail.com>
2024-07-31 02:39:41 +0600

    add product section all done

--- src/Dashboard/Dashboard/AddProduct.jsx
+++ src/Dashboard/Dashboard/AddProduct.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { toast } from "react-toastify";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOST_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -12,6 +13,11 @@ const AddProduct = () => {
     const [imgPrev, setImgPrev] = useState('')
     const axiosSecure = useAxiosSecure()
     const axiosPublic = useAxiosPublic()
+    const [feature, setFeature] = useState([])
+    const [currentFeatures, setCurrentFeatures] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState('')
+
+
 
     const handleAddProduct = async (e) => {
 
@@ -20,24 +26,44 @@ const AddProduct = () => {
         const form = e.target;
         const title = form.title.value;
 
-        const category = form.category.value;
+        const category = selectedCategory
         const rating = form.rating.value;
-        const features = form.features.value;
+        const features = feature;
         const stock = form.stock.value;
         const price = form.price.value;
         const description = form.description.value;
-        // const photo = form.photo.files[0];
+        const photo = form.photo.files[0];
 
-        // const formData = new FormData();
-        // formData.append('image', photo);
-        // const res = await axiosPublic.post(image_hosting_api, formData, {
-        //     headers: { 'Content-Type': 'multipart/form-data' }
-        // });
-        // const imgUrl = res.data.data.display_url;
+        try {
+            const formData = new FormData();
+            formData.append('image', photo);
+            const res = await axiosPublic.post(image_hosting_api, formData, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            });
+            const imgUrl = res.data.data.display_url;
 
-        //TODO: imgUrl add all property
-        const all = { title, category, rating, features, stock, price, description }
-        console.table(all)
+
+            const productData = { title, category, rating, features, stock, price, description, imgUrl }
+
+            const addProduct = await axiosSecure.post('/add-product', productData)
+            if (addProduct.data.modifiedCount === 1) {
+                toast('product added successfully')
+            }
+
+        } catch (error) {
+            console.log(error.message)
+        }
+
+
+
+    }
+
+    const handleAddFeature = () => {
+        if (currentFeatures && !feature.includes(currentFeatures)) {
+            setFeature([...feature, currentFeatures])
+            setCurrentFeatures('')
+        }
+        toast('added feature')
     }
 
     const handleImg = (e) => {
@@ -45,6 +71,10 @@ const AddProduct = () => {
         const photo = e.target.files[0];
         setImgPrev(photo.name)
 
+    }
+
+    const handleChangeCategory = (e) => {
+        setSelectedCategory(e.target.value)
     }
 
     return (
@@ -65,20 +95,46 @@ const AddProduct = () => {
                         <input id="title" name="title" type="text"
                             placeholder="title" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
                     </div>
-                    <div className="col-span-3 sm:col-span-3">
+                    <div className="col-span-3 sm:col-span-3 flex flex-col">
                         <label htmlFor="category" className="font-medium">Category</label>
+                        <select
+                            className="select focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent w-full "
+                            value={selectedCategory}
+                            onChange={handleChangeCategory}
+                        >
+                            <option disabled value="">Pick your category</option>
+                            <option value="Popular">Popular</option>
+                            <option value="NewArrival">NewArrival</option>
+                            <option value="BestSellers">BestSellers</option>
+                            <option value="TrendingGadgets">TrendingGadgets</option>
+                            <option value="SmartHome">SmartHome</option>
+                            <option value="TechEssentials">TechEssentials</option>
+                            <option value="InnovativeTech">InnovativeTech</option>
+                            <option value="GadgetDeals">GadgetDeals</option>
+                            <option value="TopRated">TopRated</option>
+                            <option value="WearableTech">WearableTech</option>
+                            <option value="HomeAutomation">HomeAutomation</option>
+                            <option value="PortableDevices">PortableDevices</option>
+                        </select>
+                        {/* <label htmlFor="category" className="font-medium">Category</label>
                         <input id="category" name="category" type="text"
-                            placeholder="category" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                            placeholder="category" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" /> */}
                     </div>
                     <div className="col-span-3 sm:col-span-3">
                         <label htmlFor="rating" className="font-medium">Rating</label>
                         <input id="rating" name="rating" type="number"
                             placeholder="rating" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
                     </div>
-                    <div className="col-span-3 sm:col-span-3">
+                    <div className="col-span-3 sm:col-span-3 ">
                         <label htmlFor="features" className="font-medium">Features</label>
-                        <input id="features" name="features" type="text"
-                            placeholder="Features" className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                        <div className="flex">
+                            <input id="features" name="features" type="text"
+                                value={currentFeatures}
+                                onChange={(e) => setCurrentFeatures(e.target.value)}
+                                placeholder="Add some Features" className="w-3/4 p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+                            <button type="button" onClick={handleAddFeature} className="ml-2 p-2 bg-blue-500 text-white rounded-md w-1/4">Add</button>
+                        </div>
+
                     </div>
                     <div className="col-span-3 sm:col-span-3">
                         <label htmlFor="stock" className="font-medium">Stock</label>
@@ -128,7 +184,7 @@ const AddProduct = () => {
                     </div>
                 </div>
                 <div className="mb-5">
-                    <button type="submit" className="btn btn-secondary w-full">Confirm Order</button>
+                    <button type="submit" className="btn btn-secondary w-full">Add Product</button>
                 </div>
             </form>
         </div>
